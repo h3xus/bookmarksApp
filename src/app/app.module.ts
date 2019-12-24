d0a1cd90599e1d8fa4b5477cf032baa1727453a4
Bartosz Cegłowski <bart@inovorobotics.com>
2019-12-24 08:54:48 +0100

    app init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BookmarkListComponent } from './bookmark-list/bookmark-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookmarkListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
